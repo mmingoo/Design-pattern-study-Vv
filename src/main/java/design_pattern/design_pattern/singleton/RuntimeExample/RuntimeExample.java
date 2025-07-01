4d0749389d327fce46e221093318aabae6001439
mmingoo <aldrn0323@naver.com>
2025-07-01 18:52:58 +0900

    싱글톤 구현

---
+++ src/main/java/design_pattern/design_pattern/singleton/RuntimeExample/RuntimeExample.java
@@ -0,0 +1,11 @@
+package design_pattern.design_pattern.singleton.RuntimeExample;
+
+public class RuntimeExample {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        // 메모리 용량
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.freeMemory());
+    }
+}
