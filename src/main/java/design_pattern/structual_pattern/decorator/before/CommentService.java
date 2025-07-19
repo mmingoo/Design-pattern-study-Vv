4ecf915c9a7ee2fed3e480b91744d60710ee0cae
mmingoo <aldrn0323@naver.com>
2025-07-19 17:23:46 +0900

    Decorator 적용 전

---
+++ src/main/java/design_pattern/structual_pattern/decorator/before/CommentService.java
@@ -0,0 +1,7 @@
+package design_pattern.structual_pattern.decorator.before;
+
+public class CommentService {
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
---
+++ src/main/java/design_pattern/structual_pattern/decorator/before/TrimmingCommentService.java
@@ -0,0 +1,14 @@
+package design_pattern.structual_pattern.decorator.before;
+
+public class TrimmingCommentService extends CommentService {
+
+    @Override
+    public void addComment(String comment) {
+        super.addComment(trim(comment));
+    }
+
+    private String trim(String comment) {
+        return comment.replace("...", "");
+    }
+
+}
